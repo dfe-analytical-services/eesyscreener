48bd70e9c476e415e7f2fae5b7dc08bd309f91da
menna.zayed@education.gov.uk <menna.zayed@education.gov.uk>
2026-01-20 22:45:06 +0000

    fixed test

--- R/check_col_snake_case.R
+++ R/check_col_snake_case.R
@@ -6,40 +6,36 @@ check_col_snake_case <- function(data, verbose = FALSE, stop_on_error = FALSE) {
     use.names = FALSE
   ))
 
+  test_name <- "col_snake_case"
+
   if (length(cap_special_char_check) == 0) {
     test_output(
-      "col_snake_case",
+      test_name,
       "PASS",
       "The variable names in the data file follow the snake_case convention.",
       verbose = verbose,
       stop_on_error = stop_on_error
     )
   } else {
-    if (length(cap_special_char_check) == 1) {
-      test_output(
-        "col_snake_case",
-        "ADVISORY",
-        paste0(
-          "The following invalid characters were found in the variable names of the data file: ",
-          paste0("'", cap_special_char_check, collapse = "', '"),
-          "'. <br> - Variable names should follow the snake_case convention and only contain lowercase letters, underscores or numbers."
-        ),
-        verbose = verbose,
-        stop_on_error = stop_on_error
-      )
-    } else {
-      test_output(
-        "col_snake_case",
+    char_word <- ifelse(
+      length(cap_special_char_check) == 1,
+      "character was",
+      "characters were"
+    )
 
-        "ADVISORY",
-        paste0(
-          "The following invalid characters were found in the variable names of the data file: ",
-          paste0("'", cap_special_char_check, collapse = "', '"),
-          "'. <br> - Variable names should follow the snake_case convention and only contain lowercase letters, underscores or numbers."
-        ),
-        verbose = verbose,
-        stop_on_error = stop_on_error
-      )
-    }
+    message <- sprintf(
+      "The following invalid %s found in the variable names of the data file: %s",
+      char_word,
+      paste0("'", paste(cap_special_char_check, collapse = "', '"), "'")
+    )
+
+    result <- "ADVISORY"
+    test_output(
+      test_name,
+      result,
+      message,
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    )
   }
 }
