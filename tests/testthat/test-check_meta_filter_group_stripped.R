cc597de0c6c52022c16b2fb168eb3c7983c5fb56
ROY <Ethan.ROY@EDUCATION.GOV.UK>
2026-01-20 16:22:13 +0000

    Writing first set of test_that() functions for check_meta_filter_group_stripped()

---
+++ tests/testthat/test-check_meta_filter_group_stripped.R
@@ -0,0 +1,80 @@
+test_that("passes when all filter groups are unique when stripped of non-alpanumeric characters", {
+  data <- data.frame(
+   sex = c("M", "F"),
+   education_phase = c("Primary", "Secondary")
+  )
+  meta <- data.frame(filter_grouping_column = c("sex", "education_phase"))
+  expect_equal(check_meta_filter_group_stripped(data, meta)$result, "PASS")
+  expect_no_error(check_meta_filter_group_stripped(
+    data,
+    meta,
+    stop_on_error = TRUE
+  ))
+  expect_no_error(check_meta_filter_group_stripped(
+    example_filter_group,
+    example_filter_group_meta,
+    stop_on_error = TRUE
+  ))
+})
+
+test_that("passes when no filter groups are defined in meta", {
+  data <- data.frame(
+    sex = c("M", "F"),
+    education_phase = c("Primary", "Secondary")
+  )
+  meta <- data.frame(
+    col_name = c("sex", "education_phase"),
+    filter_grouping_column = ""
+  )
+  expect_equal(check_meta_filter_group_stripped(data, meta)$result, "PASS")
+  expect_no_error(check_meta_filter_group_stripped(
+    data,
+    meta,
+    stop_on_error = TRUE
+  ))
+  expect_no_error(check_meta_filter_group_stripped(
+    example_data,
+    example_meta,
+    stop_on_error = TRUE
+  ))
+})
+
+test_that("ignores empty or NA filter_grouping_column values", {
+  data <- data.frame(sex = c("M", "F"))
+  meta <- data.frame(filter_grouping_column = c("sex", "", NA))
+  expect_equal(check_meta_filter_group_stripped(data, meta)$result, "PASS")
+})
+
+# Tests below don't work \/ \/ \/  -------------------------------------------
+
+test_that("fails when one filter group does not have unique stripped items", {
+  data <- data.frame(sex = c("M", "M*", "F"))
+  meta <- data.frame(filter_grouping_column = "sex")
+  expect_equal(check_meta_filter_group_stripped(data, meta)$result, "FAIL")
+  expect_error(check_meta_filter_group_stripped(data, meta, stop_on_error = TRUE))
+})
+
+test_that("fails when multiple filter groups do not have unique stripped items", {
+  data <- data.frame(
+    sex = c("M", "M*", "F"),
+    education_phase = c("Pri-mary", "Primary", "Secondary")
+  )
+  meta <- data.frame(filter_grouping_column = c("sex", "education_phase"))
+  expect_equal(check_meta_filter_group_stripped(data, meta)$result, "FAIL")
+  expect_error(check_meta_filter_group_stripped(data, meta, stop_on_error = TRUE))
+})
+
+# test_that("fails when one filter group does not have unique stripped items", {
+#   meta <- data.frame(
+#     col_name = "sex",
+#     filter_grouping_column = c("sex", "education_phase")
+#   )
+#   expect_equal(check_meta_filter_group_is_filter(meta)$result, "WARNING")
+#   expect_warning(check_meta_filter_group_is_filter(meta, stop_on_error = TRUE))
+#   expect_warning(check_meta_filter_group_is_filter(
+#     example_filter_group_meta,
+#     stop_on_error = TRUE
+#   ))
+# })
+
+
