a2b09c16b31a6c9af6a21ca2946ab47f5b6729b3
ROBINSON <Matthew3.ROBINSON@EDUCATION.GOV.UK>
2026-01-20 13:46:59 +0000

    meta ind initial add

---
+++ R/check_meta_ind_unit.R
@@ -0,0 +1,47 @@
+#' Check indicator_dp is set for all indicator rows
+#'
+#' Throw warning if there are any blank cells for indicator_dp in rows, to
+#' encourage users to explicitly specify the behaviour they want.
+#'
+#' @inheritParams precheck_meta_col_type
+#'
+#' @inherit check_filename_spaces return
+#'
+#' @family check_meta
+#'
+#' @examples
+#' check_meta_ind_unit(example_meta)
+#' check_meta_ind_unit(example_meta, verbose = TRUE)
+#' @export
+
+check_meta_ind_unit <- function(meta,
+  verbose = FALSE,
+  stop_on_error = FALSE){
+  indicator_units <- meta |>
+    dplyr::filter(
+      col_type == "Filter",
+      !is.na(indicator_unit),
+      indicator_unit != ""
+    ) |>
+    dplyr::pull(indicator_unit)
+
+  if (length(indicator_units) > 0) {
+    test_output(
+      "meta_ind_unit",
+      "FAIL",
+      "Filters should not have an indicator_unit value in the metadata file. Rows ",
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    )
+  } else {
+    test_output(
+      "meta_ind_unit",
+      "PASS",
+      "No filters have an indicator_unit value.",
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    )
+  }
+
+  return(output)
+}
