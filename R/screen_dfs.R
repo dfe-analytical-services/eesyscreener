c6e4c0163efe1b602060a5287f30053735f0e5a9
ROY <Ethan.ROY@EDUCATION.GOV.UK>
2026-01-21 12:56:10 +0000

    Add function to screen dfs

--- R/screen_dfs.R
+++ R/screen_dfs.R
@@ -143,6 +143,12 @@ screen_dfs <- function(
       verbose = verbose,
       stop_on_error = stop_on_error
     ),
+    check_meta_filter_group_stripped(
+      data,
+      meta,
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    ),
     check_meta_label(meta, verbose, stop_on_error),
     check_meta_filter_hint(meta, verbose, stop_on_error),
     check_meta_indicator_dp(meta, verbose, stop_on_error)
