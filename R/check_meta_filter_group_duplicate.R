75db48c75048b4b34ca610e669d155f04e5a2c90
CHall2342 <Christopher.HALL@education.gov.uk>
2026-02-03 15:56:01 +0000

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/check_meta_filter_group_duplicate.R
+++ R/check_meta_filter_group_duplicate.R
@@ -14,13 +14,14 @@
 #' @export
 
 check_meta_filter_group_duplicate <- function(
-    meta,
-    verbose = FALSE,
-    stop_on_error = FALSE
+  meta,
+  verbose = FALSE,
+  stop_on_error = FALSE
 ) {
   filter_groups <- meta |>
-    dplyr::filter(!is.na(filter_grouping_column),
-                  filter_grouping_column != ""
+    dplyr::filter(
+      !is.na(filter_grouping_column),
+      filter_grouping_column != ""
     ) |>
     dplyr::pull(.data$filter_grouping_column)
 
@@ -32,9 +33,7 @@ check_meta_filter_group_duplicate <- function(
       verbose = verbose,
       stop_on_error = stop_on_error
     )
-  }
-
-  else if (length(filter_groups) != dplyr::n_distinct(filter_groups)) {
+  } else if (length(filter_groups) != dplyr::n_distinct(filter_groups)) {
     test_output(
       "filter_group_duplicate",
       "FAIL",
@@ -49,5 +48,6 @@ check_meta_filter_group_duplicate <- function(
       "All of the filter_group values are unique.",
       verbose = verbose,
       stop_on_error = stop_on_error
-    )}
+    )
+  }
 }
