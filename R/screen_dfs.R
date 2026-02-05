70f118e862e0f60159333650c9632ecd6bc967e6
matthewberobinson <matthewberobinson@yahoo.com>
2026-02-05 11:32:07 +0000

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- R/screen_dfs.R
+++ R/screen_dfs.R
@@ -92,7 +92,6 @@ screen_dfs <- function(data, meta, verbose = FALSE, stop_on_error = FALSE) {
     check_meta_ind_unit(meta, verbose, stop_on_error)
   )
 
-
   check_meta_results <- precheck_meta_results |>
     rbind(
       check_meta_results |>
