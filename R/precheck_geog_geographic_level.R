a2ece56e209eaac575f9549b90dcff0bb05ad78f
ROY <Ethan.ROY@EDUCATION.GOV.UK>
2026-02-25 13:34:01 +0000

    Create geographic level validation function

--- R/precheck_geog_geographic_level.R
+++ R/precheck_geog_geographic_level.R
@@ -18,5 +18,36 @@ precheck_geog_geographic_level <- function(
   verbose = FALSE,
   stop_on_error = FALSE
 ) {
-
+  # Pull all unique geographic levels from the data set
+  present_geographic_levels <- data |>
+    dplyr::pull(geographic_level) |>
+    unique()
+  # Compare these geographic levels to the acceptable levels in geography_df
+  invalid_geographic_levels <- setdiff(
+    present_geographic_levels,
+    geography_df$geographic_level
+  )
+  # If there are no invalid geographic levels, pass the test
+  if (length(invalid_geographic_levels) == 0) {
+    test_output(
+      "geographic_level",
+      "PASS",
+      "The geographic_level values are all valid.",
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    )
+  } else {
+    # If there are any invalid geographic levels, fail the test and flag them
+    test_output(
+      "geographic_level",
+      "FAIL",
+      paste0(
+        "The following invalid geographic level(s) were found in the file: ",
+        paste0(invalid_geographic_levels, collapse = "', '"),
+        "."
+      ),
+      verbose = verbose,
+      stop_on_error = stop_on_error
+    )
+  }
 }
