b26f82b92c115746be80a9eb819e0702979f66bb
Rich Bielby <richbielby@e.email>
2026-03-20 18:02:59 +0000

    Being a bit more sensible with the robot test tests

--- tests/testthat/test-ees-robot-tests.R
+++ tests/testthat/test-ees-robot-tests.R
@@ -4,23 +4,15 @@ test_that("Check EES robot test files", {
   for (robot in ees_robot_test_data) {
     test_dir = tempdir()
     paths <- write_ees_files(robot$data, robot$meta, test_dir, robot$robot_name)
-    expect_no_error(screen_csv(paths$data, paths$meta))
+    
+    screener_output <- expect_no_error(screen_csv(paths$data, paths$meta))
+
+    # Now test that all results are either PASS, WARNING or FAIL
+    results <- screener_output$results$result |> unique()
+        expect_true(      all(results %in% c("PASS", "WARNING", "FAIL")))
+
     file.remove(paths$data_path)
     file.remove(paths$meta_path)
   }
 })
 
-# Checking that the screener runs to completion on all robot tests
-test_that("Test that all checks only produce one of PASS / FAIL / WARNING when run with EES robot tests", {
-  test_dir <- tempdir()
-  for (robot in ees_robot_test_data) {
-    test_dir = tempdir()
-    paths <- write_ees_files(robot$data, robot$meta, test_dir, robot$robot_name)
-    results <- screen_csv(paths$data, paths$meta)$results$result |> unique()
-    expect_true(
-      all(results %in% c("PASS", "WARNING", "FAIL"))
-    )
-    file.remove(paths$data_path)
-    file.remove(paths$meta_path)
-  }
-})
