3d4f0c4846414809bdf4ce8268de08eb6b2ab7ce
Rich Bielby <richard.bielby@education.gov.uk>
2026-03-23 17:05:42 +0000

    Clearing out non-accepted test results (#49)
    * Clearing out non-accepted test results and updating tests to catch any more
    
    * Being a bit more sensible with the robot test tests
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Removed advisory reference in testing
    
    * Data dictionary update
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-ees-robot-tests.R
+++ tests/testthat/test-ees-robot-tests.R
@@ -4,8 +4,12 @@ test_that("Check EES robot test files", {
   for (robot in ees_robot_test_data) {
     test_dir = tempdir()
     paths <- write_ees_files(robot$data, robot$meta, test_dir, robot$robot_name)
+    screener_output <- expect_no_error(screen_csv(paths$data, paths$meta))
 
-    expect_no_error(screen_csv(paths$data, paths$meta))
+    # Now test that all results are either PASS, WARNING or FAIL
+    results <- screener_output$results$result |> unique()
+    expect_true(all(results %in% c("PASS", "WARNING", "FAIL")))
+
     file.remove(paths$data_path)
     file.remove(paths$meta_path)
   }
