50e47bc1785035586cfb0f4725e28dec14864946
Rich Bielby <richbielby@e.email>
2026-03-25 12:25:41 +0000

    Added draft function for users to identify duplicate rows (outside of check suite).

---
+++ R/find_duplicate_rows.R
@@ -0,0 +1,21 @@
+#' Identify duplicate rows
+#'
+#' Identify duplicate rows in a data set
+#'
+#' @inheritParams precheck_col_req_data
+#'
+#' @examples
+#' find_duplicate_rows(bind_rows(example_data, example_data), example_meta)
+#' @export
+find_duplicate_rows <- function(data, meta){
+  columns <- c(
+    "time_period",
+    "time_identifier",
+    "geographic_level",
+    names(data)[names(data) %in% c(geography_df$name_field, geography_df$code_field)],
+    get_cols_meta(meta, grouping_cols = TRUE)
+  )
+  data |> 
+    dplyr::summarise(row_count = dplyr::n(), .by = columns) |>
+    dplyr::filter(row_count > 1)
+}
