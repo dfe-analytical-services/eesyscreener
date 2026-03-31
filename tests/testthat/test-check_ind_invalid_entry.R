353c675bdd6e4c95436c8adc520f5f272305d9d5
FOSTER <Charlotte.FOSTER@education.gov.uk>
2026-03-31 14:50:17 +0100

    rename & update the tests accordingly

--- tests/testthat/test-check_ind_invalid_entry.R
+++ tests/testthat/test-check_ind_invalid_entry.R
@@ -1,23 +1,23 @@
-testthat::test_that("check_ind_blanks: passes when there are no blanks in indicators", {
-  # Should not error when there are no blanks in the indicators
+testthat::test_that("check_ind_invalid_entry: passes when there are invalid entries in indicators", {
+  # Should not error when there are invalid entries in the indicators
   expect_no_error(
-    check_ind_blanks(example_data, example_meta)
+    check_ind_invalid_entry(example_data, example_meta)
   )
 })
 
-testthat::test_that("check_ind_blanks: fails when there are blanks in indicators", {
+testthat::test_that("check_ind_invalid_entry: fails when there are blanks in indicators", {
   # Create a copy of the example data with a blank value in the indicator column
   example_data_with_blank <- example_data
   example_data_with_blank$enrolment_count[1] <- ""
 
   # Should error when there are blanks in the indicators
   expect_equal(
-    check_ind_blanks(example_data_with_blank, example_meta)$result,
+    check_ind_invalid_entry(example_data_with_blank, example_meta)$result,
     "FAIL"
   )
 })
 
-testthat::test_that("check_ind_blanks: fails when there are blanks in indicators not in the first row", {
+testthat::test_that("check_ind_invalid_entry: fails when there are blanks in indicators not in the first row", {
   # Create a copy of the example data with a blank value in the indicator column
   example_data_with_blank <- example_data
   example_data_with_blank$enrolment_count[7] <- ""
@@ -25,7 +25,7 @@ testthat::test_that("check_ind_blanks: fails when there are blanks in indicators
 
   # Should error when there are blanks in the indicators
   expect_equal(
-    check_ind_blanks(example_data_with_blank, example_meta)$result,
+    check_ind_invalid_entry(example_data_with_blank, example_meta)$result,
     "FAIL"
   )
 })
