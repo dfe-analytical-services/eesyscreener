ea3375ed4d38f529112dec4b4bea83435e6d0286
FOSTER <Charlotte.FOSTER@education.gov.uk>
2026-03-31 14:52:19 +0100

    add all tests into one script under one name

--- tests/testthat/test-check_ind_invalid_entry.R
+++ tests/testthat/test-check_ind_invalid_entry.R
@@ -29,3 +29,47 @@ testthat::test_that("check_ind_invalid_entry: fails when there are blanks in ind
     "FAIL"
   )
 })
+
+testthat::test_that("check_ind_invalid_entry: passes when there are no obsolete symbols in indicators and indicators contain acceptable symbols", {
+  # Create a copy of the example data with a symbol that is not obsolete in the indicator column
+  example_data_with_acceptable_symbol <- example_data
+  example_data_with_acceptable_symbol$enrolment_count[1] <- "x"
+
+  # Should not error when there are no obsolete symbols in the indicators, even if there are other symbols present
+  expect_no_error(
+    check_ind_invalid_entry(
+      example_data_with_acceptable_symbol,
+      example_meta
+    )
+  )
+})
+
+testthat::test_that("check_ind_invalid_entry: advisory when there is an ~ obsolete symbol in indicators", {
+  # Create a copy of the example data with an obsolete symbol in the indicator column
+  example_data_with_obsolete_symbol <- example_data
+  example_data_with_obsolete_symbol$enrolment_count[1] <- "~"
+
+  # Should return advisory when there are obsolete symbols in the indicators
+  expect_equal(
+    check_ind_invalid_entry(
+      example_data_with_obsolete_symbol,
+      example_meta
+    )$result,
+    "FAIL"
+  )
+})
+
+testthat::test_that("check_ind_invalid_entry: advisory when there is an : obsolete symbol in indicators", {
+  # Create a copy of the example data with an obsolete symbol in the indicator column
+  example_data_with_obsolete_symbol <- example_data
+  example_data_with_obsolete_symbol$enrolment_count[8] <- ":"
+
+  # Should return advisory when there are obsolete symbols in the indicators
+  expect_equal(
+    check_ind_invalid_entry(
+      example_data_with_obsolete_symbol,
+      example_meta
+    )$result,
+    "FAIL"
+  )
+})
