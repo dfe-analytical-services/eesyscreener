8c7fe4765946b82f3ea68b3336f12c39ba0bfd7f
cjrace <52536248+cjrace@users.noreply.github.com>
2026-04-14 00:08:10 +0100

    So far, so good!

--- tests/testthat/test-zzz_integration.R
+++ tests/testthat/test-zzz_integration.R
@@ -1,4 +1,3 @@
-# Intentionally running these last as they will take the longest to run.
 # These are effectively integration tests, throwing CSVs as the whole
 # package to ensure that the screening process works as expected.
 #
@@ -7,22 +6,20 @@
 #     c(RUN_INTEGRATION_TESTS = "true"),
 #     devtools::test(filter = "zzz_integration")
 #   )
-test_that("Check file_validation files all fail screening", {
-  skip_on_cran()
-  skip_on_ci()
-  skip_if(
-    Sys.getenv("RUN_INTEGRATION_TESTS") != "true",
-    message = "Set RUN_INTEGRATION_TESTS=true to run integration tests"
-  )
-  skip_if_offline()
 
+# Helper: downloads and screens all data/meta CSV pairs in a GitHub folder.
+# If check_fails = TRUE, also asserts that every file produces passed = FALSE.
+screen_github_folder <- function(folder, check_fails = TRUE) {
   api_url <- paste0(
     "https://api.github.com/repos/dfe-analytical-services/",
-    "dfe-published-data-qa/contents/tests/testthat/fileValidation"
+    "dfe-published-data-qa/contents/tests/testthat/",
+    folder
   )
   base_url <- paste0(
     "https://raw.githubusercontent.com/dfe-analytical-services/",
-    "dfe-published-data-qa/refs/heads/main/tests/testthat/fileValidation/"
+    "dfe-published-data-qa/refs/heads/main/tests/testthat/",
+    folder,
+    "/"
   )
 
   all_files <- jsonlite::fromJSON(api_url)$name
@@ -60,12 +57,45 @@ test_that("Check file_validation files all fail screening", {
     }
 
     screener_output <- expect_no_error(screen_csv(data_path, meta_path))
-    expect_false(
-      screener_output$passed,
-      label = paste("passed is FALSE for", filename)
-    )
+
+    if (check_fails) {
+      expect_false(
+        screener_output$passed,
+        label = paste("passed is FALSE for", filename)
+      )
+    }
 
     file.remove(data_path)
     if (file.exists(meta_path)) file.remove(meta_path)
   }
+}
+
+integration_skip_conditions <- function() {
+  skip_on_cran()
+  skip_on_ci()
+  skip_if(
+    Sys.getenv("RUN_INTEGRATION_TESTS") != "true",
+    message = "Set RUN_INTEGRATION_TESTS=true to run integration tests"
+  )
+  skip_if_offline()
+}
+
+test_that("Check file_validation files all fail screening", {
+  integration_skip_conditions()
+  screen_github_folder("fileValidation", check_fails = TRUE)
+})
+
+test_that("Check preCheck1 files all fail screening", {
+  integration_skip_conditions()
+  screen_github_folder("preCheck1", check_fails = TRUE)
+})
+
+test_that("Check preCheck2 files all fail screening", {
+  integration_skip_conditions()
+  screen_github_folder("preCheck2", check_fails = TRUE)
+})
+
+test_that("Check mainTests files all screen without error", {
+  integration_skip_conditions()
+  screen_github_folder("mainTests", check_fails = FALSE)
 })
