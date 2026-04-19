09fd9c623ef46df74071c6e56e8ebc47acf3804b
cjrace <52536248+cjrace@users.noreply.github.com>
2026-04-19 14:59:36 +0100

    Forgot to format

--- tests/testthat/test-zzz_integration.R
+++ tests/testthat/test-zzz_integration.R
@@ -23,18 +23,15 @@ test_that("All pass-data files return passed = TRUE", {
   screen_local_folder("pass-data", expected_passed = TRUE)
 })
 
-test_that(
-  "All files in not-api-data return passed = TRUE and api_suitable = FALSE",
-  {
-    skip_if(
-      identical(Sys.getenv("SKIP_INTEGRATION_TESTS"), "true"),
-      "Set SKIP_INTEGRATION_TESTS=false or unset to run integration tests"
-    )
-    skip_if(!dir.exists(test_path("not-api-data")))
-    screen_local_folder(
-      "not-api-data",
-      expected_passed = TRUE,
-      expected_api_suitable = FALSE
-    )
-  }
-)
+test_that("All files in not-api-data return passed = TRUE and api_suitable = FALSE", {
+  skip_if(
+    identical(Sys.getenv("SKIP_INTEGRATION_TESTS"), "true"),
+    "Set SKIP_INTEGRATION_TESTS=false or unset to run integration tests"
+  )
+  skip_if(!dir.exists(test_path("not-api-data")))
+  screen_local_folder(
+    "not-api-data",
+    expected_passed = TRUE,
+    expected_api_suitable = FALSE
+  )
+})
